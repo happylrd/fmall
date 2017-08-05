2caf77c3db16f97781510020e314f0e56af07d4b
happylrd <happylrd17@gmail.com>
2017-08-05 18:48:26 +0800

    Add icon font

--- FMall/juno-core/src/main/java/io/happylrd/juno/core/app/Configurator.java
+++ FMall/juno-core/src/main/java/io/happylrd/juno/core/app/Configurator.java
@@ -1,10 +1,16 @@
 package io.happylrd.juno.core.app;
 
+import com.joanzapata.iconify.IconFontDescriptor;
+import com.joanzapata.iconify.Iconify;
+
+import java.util.ArrayList;
 import java.util.WeakHashMap;
 
 public class Configurator {
 
     private static final WeakHashMap<String, Object> JUNO_CONFIGS = new WeakHashMap<>();
+
+    private static final ArrayList<IconFontDescriptor> ICONS = new ArrayList<>();
 
     private Configurator() {
         JUNO_CONFIGS.put(ConfigType.CONFIG_READY.name(), false);
@@ -23,11 +29,26 @@ public class Configurator {
     }
 
     public final void configure() {
+        initIcons();
         JUNO_CONFIGS.put(ConfigType.CONFIG_READY.name(), true);
     }
 
     public final Configurator withApiHost(String host) {
         JUNO_CONFIGS.put(ConfigType.API_HOST.name(), host);
+        return this;
+    }
+
+    private void initIcons() {
+        if (ICONS.size() > 0) {
+            final Iconify.IconifyInitializer initializer = Iconify.with(ICONS.get(0));
+            for (int i = 1; i < ICONS.size(); i++) {
+                initializer.with(ICONS.get(i));
+            }
+        }
+    }
+
+    public final Configurator withIcon(IconFontDescriptor descriptor) {
+        ICONS.add(descriptor);
         return this;
     }
 
--- FMall/juno/src/main/java/io/happylrd/fmall/juno/ExampleApp.java
+++ FMall/juno/src/main/java/io/happylrd/fmall/juno/ExampleApp.java
@@ -2,7 +2,10 @@ package io.happylrd.fmall.juno;
 
 import android.app.Application;
 
+import com.joanzapata.iconify.fonts.FontAwesomeModule;
+
 import io.happylrd.juno.core.app.Juno;
+import io.happylrd.juno.ec.icon.FontEcModule;
 
 public class ExampleApp extends Application {
 
@@ -10,6 +13,8 @@ public class ExampleApp extends Application {
     public void onCreate() {
         super.onCreate();
         Juno.init(this)
+                .withIcon(new FontAwesomeModule())
+                .withIcon(new FontEcModule())
                 .withApiHost("http://127.0.0.1/")
                 .configure();
     }
